f48c1f897c37b14efa66e47e86a69c87d036f389
tumon <mofakhar0@gmail.com>
2023-12-14 02:09:41 +0600

    .

--- src/ui/button.ts
+++ src/ui/button.ts
@@ -2,8 +2,23 @@ import * as stylex from "@stylexjs/stylex";
 
 export const button = stylex.create({
   base: {
-    fontSize: 26,
-    lineHeight: 1.5,
+    fontSize: {
+      default: 26,
+      "@media (max-width: 800px)": 16,
+    },
+    borderRadius: "8px",
+    border: "1px",
+    borderColor: {
+      default: "transparent",
+      ":hover": "#646cff",
+    },
+    borderStyle: "solid",
+    padding: "0.6em 1.2em",
+    fontWeight: 500,
+    fontFamily: "inherit",
+    backgroundColor: "#1a1a1a",
+    cursor: "pointer",
+    transition: "border-color 0.25s",
   },
   highlighted: {
     color: "rebeccapurple",
