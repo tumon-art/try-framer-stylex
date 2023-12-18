fab9ec76bba61e1e9e9888f0f26595c6a940d951
Tumon <53380504+tumon-art@users.noreply.github.com>
2023-12-18 20:11:07 +0000

    .

--- src/comps/Otp.tsx
+++ src/comps/Otp.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { ChangeEvent, useRef, useState } from "react";
 import * as stylex from "@stylexjs/stylex";
 
 const inputPer = stylex.create({
@@ -24,6 +24,36 @@ const inputBox = stylex.create({
 });
 export default function Otp() {
   const [otp, setOtp] = useState(["", "", "", ""]);
+	const otpFields = [
+		useRef<HTMLInputElement>(null),
+		useRef<HTMLInputElement>(null),
+		useRef<HTMLInputElement>(null),
+		useRef<HTMLInputElement>(null),
+	];
+
+
+  const inputChange = (e:ChangeEvent<HTMLInputElement>, index: number) => {
+		const value = e.target.value;
+
+    if (/^\d*$/.test(value) && value.length <= 1) {
+      otp[index] = value
+      setOtp([...otp])
+      if(value !== "" && index < 3) {
+        otpFields[index + 1].current?.focus();
+      }
+    }
+
+  }
+
+  function inputBackspaceAndEnter(e:any, index:number) {
+    const value = e.target.value;
+    if(e.key === "Backspace" && !value && index > 0){
+      otpFields[index - 1].current?.focus();
+    }
+    if(e.key === "Enter" && value && index < otp.length-1){
+      otpFields[index + 1].current?.focus();
+    }
+  }
 
   return (
     <form>
@@ -32,8 +62,14 @@ export default function Otp() {
           <input
             {...stylex.props(inputBox.base)}
             key={i}
-            // value={each}
+            placeholder="-"
+            ref={otpFields[i]}
+            value={each}
             type="text"
+            onChange={(e) => inputChange(e, i)}
+            onKeyUp={(e)=> inputBackspaceAndEnter(e, i)}
+            maxLength={1}
+            pattern="[0-9]*"
           />
         ))}
       </section>
