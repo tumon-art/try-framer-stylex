eaedb1abe69188c3923cf58a17e18f593111abe9
Tumon <53380504+tumon-art@users.noreply.github.com>
2023-12-18 03:06:58 +0000

    .

--- src/comps/Otp.tsx
+++ src/comps/Otp.tsx
@@ -1,9 +1,37 @@
 import { useState } from "react";
+import * as stylex from "@stylexjs/stylex";
 
-export default function Otp(){
-    const [otp, setOtp] = useState(["", "", "", ""]);
+const inputPer = stylex.create({
+  base: {
+    display: "flex",
+    justifyContent: "center",
+    gap: "5px",
+  },
+});
 
-    return <form>
-        {otp.map((each,i)=> <input value={each} type="text" />)}
+const inputBox = stylex.create({
+  base: {
+    width: "15px",
+    height: "30px",
+    border: "2px solid #00d8ff",
+    borderRadius: "5px",
+  },
+});
+export default function Otp() {
+  const [otp, setOtp] = useState(["", "", "", ""]);
+
+  return (
+    <form>
+      <section {...stylex.props(inputPer.base)}>
+        {otp.map((each, i) => (
+          <input
+            {...stylex.props(inputBox.base)}
+            key={i}
+            value={each}
+            type="text"
+          />
+        ))}
+      </section>
     </form>
+  );
 }
